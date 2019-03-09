56a105dd2ec8aa04975d45caf9e23ef2ba7fdd42
orwell96 <orwell@bleipb.de>
2019-03-09 09:58:17 +0100

    Restore old door status display behavior

--- advtrains/trainhud.lua
+++ advtrains/trainhud.lua
@@ -203,7 +203,7 @@ function advtrains.hud_train_format(train, flip)
 	end
 	local shtind = train.is_shunt and "S" or ">"
 	
-	topLine="  ["..mletter[fct].."]  {"..levers.."} "..doorstr[(train.door_open or 0) * fct].."  "..(train.line and "L: "..train.line or "")
+	topLine="  ["..mletter[fct].."]  {"..levers.."} "..doorstr[(train.door_open or 0)].."  "..(train.line and "L: "..train.line or "")
 	firstLine=attrans("Speed:").." |"..string.rep("+", vel)..string.rep("_", res-vel).."|"..string.rep("_", max-res)..shtind.." "..vel_kmh.." km/h"
 	if train.speed_restriction == 0 then
 		firstLine = "OVERRUN RED SIGNAL! Examine situation and reverse train to move again."
