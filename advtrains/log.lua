8d794525b3232dbe326043ee3d219ecabee04636
orwell96 <orwell@bleipb.de>
2019-12-04 10:08:03 +0100

    Fix repeated log file opening/closing (H#136)

--- advtrains/log.lua
+++ advtrains/log.lua
@@ -5,9 +5,13 @@ advtrains.log = function() end
 if minetest.settings:get_bool("advtrains_enable_logging") then
 	advtrains.logfile = advtrains.fpath .. "_log"
 
+	local log = io.open(advtrains.logfile, "a+")
+	
 	function advtrains.log (event, player, pos, data)
-	   local log = io.open(advtrains.logfile, "a+")
 	   log:write(os.date()..": "..event.." by "..player.." at "..minetest.pos_to_string(pos).." -- "..(data or "").."\n")
-	   log:close()
 	end
+	
+	minetest.register_on_shutdown(function()
+		log:close()
+	end)
 end
