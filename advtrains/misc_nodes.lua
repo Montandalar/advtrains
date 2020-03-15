301dcf953e9a73d0e92f1a48af141200891f0e55
Blockhead <jbis1337@hotmail.com>
2020-03-15 21:56:42 +1100

    Reduce the profile of the platforms by 0.1
    Currently some trains clip the platform (especially E231
    modpack), but they won't after the platform edge is moved
    back slightly.

--- advtrains/misc_nodes.lua
+++ advtrains/misc_nodes.lua
@@ -38,8 +38,8 @@ function advtrains.register_platform(modprefix, preset)
 		node_box = {
 			type = "fixed",
 			fixed = {
-				{-0.5,  0.3, -0.1, 0.5,  0.5, 0.5},
-				{-0.5, -0.5,  0  , 0.5,  0.3, 0.5}
+				{-0.5,  0.3, 0, 0.5,  0.5, 0.5},
+				{-0.5, -0.5, 0.1  , 0.5,  0.3, 0.5}
 			},
 		},
 		paramtype2="facedir",
