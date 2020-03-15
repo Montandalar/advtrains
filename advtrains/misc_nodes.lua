09fda4ffaa88ec5931aebc45c649cf808efbd813
Blockhead <jbis1337@hotmail.com>
2020-03-15 21:41:53 +1100

    Add 45 degree diagonal platforms with a mesh

--- advtrains/misc_nodes.lua
+++ advtrains/misc_nodes.lua
@@ -46,6 +46,28 @@ function advtrains.register_platform(modprefix, preset)
 		paramtype = "light",
 		sunlight_propagates = true,
 	})
+	local diagonalbox = {
+			type = "fixed",
+			fixed = {
+				{-0.5,  -0.5, 0.5, -0.25, 0.5, -0.8 },
+				{-0.25, -0.5, 0.5 , 0,    0.5, -0.55},
+				{0,     -0.5, 0.5 , 0.25, 0.5, -0.3 },
+				{0.25 , -0.5, 0.5,  0.5,  0.5, -0.05}
+			}
+	}
+	minetest.register_node(modprefix..":platform_45_"..nodename, {
+		description = attrans("@1 Platform (45 degree)", desc),
+		groups = {cracky = 1, not_blocking_trains = 1, platform=2},
+		sounds = default.node_sound_stone_defaults(),
+		drawtype = "mesh",
+		mesh = "advtrains_platform_diag.b3d",
+		selection_box = diagonalbox,
+		collision_box = diagonalbox,
+		tiles = {btex, btex.."^advtrains_platform_diag.png"},
+		paramtype2 = "facedir",
+		paramtype = "light",
+		sunlight_propagates = true,
+	})
 	minetest.register_craft({
 		type="shapeless",
 		output = modprefix .. ":platform_high_"..nodename.." 4",
@@ -59,6 +81,13 @@ function advtrains.register_platform(modprefix, preset)
 		recipe = {
 			"dye:yellow", preset
 		},
+	})
+	minetest.register_craft({
+		type="shapeless",
+		output = modprefix .. ":platform_45_"..nodename.." 2",
+		recipe = {
+			"dye:yellow", preset, preset, preset
+		}
 	})
 end
 
