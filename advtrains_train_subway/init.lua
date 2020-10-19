423dc8e716d87012901b2dbfb49ea867f16bdf10
Blockhead <jbis1337@hotmail.com>
2020-10-19 21:58:44 +1100

    Start subway livery feature
    TODO: preserve line # when painting.

--- advtrains_train_subway/init.lua
+++ advtrains_train_subway/init.lua
@@ -1,5 +1,30 @@
 local S = attrans
 
+-- Gets called one, currently when punched with bike painter
+local function subway_set_livery(self, puncher, itemstack,data)
+	-- Get color data from the bike painter
+	local meta = itemstack:get_meta()
+	local color = meta:get_string("paint_color")
+	local alpha = tonumber(meta:get_string("alpha"))
+	if color and color:find("^#%x%x%x%x%x%x$") then
+		if alpha == 0 then
+			data.livery = "advtrains_subway_wagon.png"
+		else 
+			data.livery = "advtrains_subway_wagon.png^(advtrains_subway_wagon_livery.png^[colorize:"..color..":"..alpha..")^advtrains_subway_wagon_extoverlay.png" -- livery texture has no own texture....
+		end
+		self:set_textures(data)
+	end
+end
+
+-- Gets called when an entity is made - will set the right livery that was painted
+local function subway_set_textures(self, data)
+	if data.livery then
+		self.object:set_properties({
+				textures={data.livery}
+		})
+	end
+end
+
 advtrains.register_wagon("subway_wagon", {
 	mesh="advtrains_subway_wagon.b3d",
 	textures = {"advtrains_subway_wagon.png"},
@@ -97,48 +122,60 @@ advtrains.register_wagon("subway_wagon", {
 	end,
 	custom_on_step = function(self, dtime, data, train)
 		--set line number
-		local line = nil
-		if train.line and self.line_cache ~= train.line then
-			self.line_cache=train.line
-			local lint = train.line
-			if string.sub(train.line, 1, 1) == "S" then
-				lint = string.sub(train.line,2)
+		if train.line and self.line_cache == train.line then
+			return
+		end
+
+		local line = train.line
+		self.line_cache = line
+
+		if line == nil then
+			return
+		end
+		if line:find("[SsUuEe]", 1) == 1 then
+			line = line:sub(2)
+		end
+		local newtex = data.livery or "advtrains_subway_wagon.png"
+		local int = tonumber(line)
+		if int == nil then
+			if line:find("[xX]") then
+				-- X texture
+				newtex = newtex .. "^advtrains_subway_wagon_lineX.png"
+			else
+				newtex = nil
 			end
-			if string.len(lint) == 1 then
-				if lint=="X" then line="X" end
-				line = tonumber(lint)
-			elseif string.len(lint) == 2 then
-				if tonumber(lint) then
-					line = lint
+		else
+			local strlen = #line
+			if strlen == 1 then
+				-- Texture 0-9
+				newtex = string.format("%s^advtrains_subway_wagon_line%d.png", newtex, line)
+			elseif strlen == 2 then
+				-- Hume2's algorithm for 2 digits
+				local num = tonumber(line)
+				local red = math.fmod(line*67+101, 255)
+				local green = math.fmod(line*97+109, 255)
+				local blue = math.fmod(line*73+127, 255)
+				newtex = string.format(
+					"%s^(advtrains_subway_wagon_line.png^[colorize:#%X%X%X%X%X%X)^(advtrains_subway_wagon_line%s_.png^advtrains_subway_wagon_line_%s.png",
+					newtex, math.floor(red/16), math.fmod(red,16), math.floor(green/16), math.fmod(green,16), math.floor(blue/16), math.fmod(blue,16),
+					string.sub(line, 1, 1), string.sub(line, 2, 2)
+				)
+				if red + green + blue > 512 then
+					newtex = newtex .. "^[colorize:#000"
 				end
+				newtex = newtex .. ")"
+			else
+				-- No texture
+				newtex = nil
 			end
-			if line then
-				local new_line_tex="advtrains_subway_wagon.png"
-				if type(line)=="number" or line == "X" then
-					new_line_tex = new_line_tex.."^advtrains_subway_wagon_line"..line..".png"
-				else
-					local num = tonumber(line)
-					local red = math.fmod(line*67+101, 255)
-					local green = math.fmod(line*97+109, 255)
-					local blue = math.fmod(line*73+127, 255)
-					new_line_tex = new_line_tex..string.format("^(advtrains_subway_wagon_line.png^[colorize:#%X%X%X%X%X%X)^(advtrains_subway_wagon_line%s_.png^advtrains_subway_wagon_line_%s.png", math.floor(red/16), math.fmod(red,16), math.floor(green/16), math.fmod(green,16), math.floor(blue/16), math.fmod(blue,16), string.sub(line, 1, 1), string.sub(line, 2, 2))
-					if red + green + blue > 512 then
-						new_line_tex = new_line_tex .. "^[colorize:#000)"
-					else
-						new_line_tex = new_line_tex .. ")"
-					end
-				end
-				self.object:set_properties({
-						textures={new_line_tex},
-				})
-			elseif self.line_cache~=nil and line==nil then
-				self.object:set_properties({
-						textures=self.textures,
-				})
-				self.line_cache=nil
-			end
-		end	
-	end,
+		end
+		
+		if (newtex ~= nil) then
+			self.object:set_properties({textures = {newtex}})
+		end
+	end,--[[ --]]
+	set_textures = subway_set_textures,
+	set_livery = subway_set_livery,
 }, S("Subway Passenger Wagon"), "advtrains_subway_wagon_inv.png")
 
 --wagons
